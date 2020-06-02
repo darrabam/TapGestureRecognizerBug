4013358a76f18562c13abc9758897d3ea1be53f8
Aziz Al-Darrab <adarrab@gmail.com>
2020-06-02 06:34:46 +0300

    Add project files.

---
+++ TapGestureRecognizerBug/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TapGestureRecognizerBug
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new PersonViewModel();
+        }
+
+        private async void OnGridClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Tap Detected!", "Tap Detected!", "ok");
+        }
+    }
+}
---
+++ TapGestureRecognizerBug/PersonViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TapGestureRecognizerBug
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand DeleteCommand => new Command<Person>(DeletePerson);
+        public ObservableCollection<Person> People { get; private set; } = new ObservableCollection<Person>();
+        public PersonViewModel()
+        {
+            People.Add(new Person("Steve", 21));
+            People.Add(new Person("John", 42));
+            People.Add(new Person("Tom", 29));
+            People.Add(new Person("Lucas", 29));
+            People.Add(new Person("Jane", 30));
+            OnPropertyChanged("PersonViewModel");
+        }
+        private void DeletePerson(Person obj)
+        {
+            Xamarin.Forms.Application.Current.MainPage.DisplayAlert("DeleteCommand", obj?.Name, "Ok");
+            OnPropertyChanged("DeletePerson");
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
